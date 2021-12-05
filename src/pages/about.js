c270948620181f8e3f93ac55b23a77f061496888
Jerry Frederic <jfpl.dev@gmail.com>
2021-12-05 22:59:16 +0100

    feat(app): set about page

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+const AboutPage = () => {
+  return (
+    <main>
+      <title>About Me</title>
+      <h1>About Me</h1>
+      <p>
+        Hi there! I'm the proud creator of this site, which I built with Gatsby.
+        If you want to know a little more about me just click here{" "}
+        <a href={"www.jfpl.fr"}>jfpl.fr</a>
+      </p>
+    </main>
+  );
+};
+
+export default AboutPage;
